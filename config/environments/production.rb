6c724c523d0f953cc636bbce8677da7480fd6e57
John Hogarty <hogihung@gmail.com>
2014-03-21 19:07:00 -0400

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Checkpoint Interlude
+  config.action_mailer.default_url_options = { host: 'jhogarty-bloccit.herokuapp.com' }
 end
