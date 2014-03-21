b0b6cb114d2d380d17d61ba359b6a4224a4cedd5
John Hogarty <hogihung@gmail.com>
2014-03-20 20:39:35 -0400

    Associated and nested topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,46 +1,46 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-    authorize @posts
-  end
 
   def show
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize @post
   end
 
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
   def create
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
-    #raise # this will short-circuit the method, helped with debugging.
+    @post.topic = @topic
+    
     authorize @post
     if @post.save
-      flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post], notice: "Post was saved successfully."
     else
-      flash[:error] = "There was an error saving the post.  Please try again."
+      flash[:error] = "There was an error creating the post.  Please try again."
       render :new
     end
   end
 
-  def edit
-    @post = Post.find(params[:id])
-    authorize @post
-  end
-
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post.  Pleast try again."
       render :edit
     end 
   end
-  
 end
