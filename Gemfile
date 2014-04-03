dfa430bd0207e789110fe8d96a26cd05c9cbd44e
Hashrocket Workstation <dev@hashrocket.com>
2014-04-02 21:42:18 -0400

    Wrote test spec for User#top_rated

--- Gemfile
+++ Gemfile
@@ -72,5 +72,6 @@ gem 'will_paginate', '~> 3.0.5'
 group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails', '~> 4.0'
+  gem 'database_cleaner'
 end
 
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name     "Doughlas Adams"
-    email    "doughlas@example.com"
+    sequence(:email) { |n| "person{n}@example.com" }
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
