c9c62ee2cf0e5ea90f281d888fd64d4751f877f5
Hashrocket Workstation <dev@hashrocket.com>
2014-04-06 14:26:03 -0400

    Ajaxified comment destruction

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
+
   def create
     @comment = current_user.comments.build(post_params)
-    
+
     authorize @comment
     if @comment.save
       redirect_to [@comment.post.topic, @comment.post], notice: "Comment was successfully added."
@@ -9,23 +11,26 @@ class CommentsController < ApplicationController
       flash[:error] = "There was an error creating the comment.  Please try again."
       render :new
     end
+  end
 
-    def destroy
-      @topic = Topic.find(params[:topci_id])
-      @post  = @topic.posts.find(params[:post_id])
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post  = @topic.posts.find(params[:post_id])
 
-      @comment = @post.comments.find(params[:id])
+    @comment = @post.comments.find(params[:id])
+    authorize @comment
 
-      authorize @comment
-      if @comment.destroy
-        flash[:notice] = "Comment was removed."
-        redirect_to [@topic, @post]
-      else
-        flash[:error] = "Comment couldn't be deleted.  Try again"
-        redirect_to [@topic, @post]
-      end
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      #redirect_to [@topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted.  Try again"
+      #redirect_to [@topic, @post]
     end
 
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
+    end
   end
 
 
@@ -34,4 +39,5 @@ class CommentsController < ApplicationController
   def post_params
     params.require(:comment).permit(:body, :post_id)
   end
+
 end
